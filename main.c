9869fbb52a45bb1ef247c21d25cec4381c6d3b92
Валерий Иванов <valeraivanov2681@gmail.com>
2026-02-01 16:10:41 +0300

    main writed

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
 #include "inc/common.h"
+#include "inc/encoder.h"
+#include "inc/lcd.h"
+#include "inc/PWM.h"
+#include "inc/rgbLed.h"
+
+void allInit(void);
+extern uint8_t* str;
+
+int main(void)
+{
+  allInit();
+
+  while(1)
+  {
+    lcdWrite(str, );
+  }
+}
+
+void allInit(void)
+{
+  PWMInit();
+  lcdInit();
+  encoderInit();
+  rgbLedInit();
+  
+  sei();
+}
